2f7883d7673a2f8539f180a4f61e09ae4f683f35
Dries Deboosere <dries.deboosere@howest.be>
2019-09-09 18:12:09 +0200

    Add project files.

---
+++ EnumIntro.WPF/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace EnumIntro.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        enum Seizoen { Herfst, Winter, Lente, Zomer }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnToonEnumWaarde_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(Seizoen.Zomer.ToString());
+        }
+    }
+}

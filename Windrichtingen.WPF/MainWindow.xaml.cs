3395116b3647e8b8644c8ad314564efcb1c1295f
Dries Deboosere <dries.deboosere@howest.be>
2019-09-10 13:46:29 +0200

    Add example for windrichtingen

---
+++ Windrichtingen.WPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Windrichtingen.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        enum WindRichting { Oost, Zuid, Noord, West };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnToonVoorspelling_Click(object sender, RoutedEventArgs e)
+        {
+            int huidigeWindNr = (int)WindRichting.Oost;
+            string huidigeWind = WindRichting.Oost.ToString();
+            lblVoorspelling.Content = "Huidige wind = " + huidigeWind + " (nr. " + huidigeWindNr + ")\n";
+
+            int windVoorMorgenNr = (int)WindRichting.West;
+            string windVoorMorgen = WindRichting.West.ToString();
+            lblVoorspelling.Content += "Wind voor morgen = " + windVoorMorgen + " (nr. " + windVoorMorgenNr + ")\n";
+
+            WindRichting richtingNul = (WindRichting)0;
+            lblVoorspelling.Content += "Wind met index 0 = " + richtingNul.ToString();
+        }
+    }
+}
